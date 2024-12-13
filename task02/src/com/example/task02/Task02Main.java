925efabf060325d258ba5473d3fbc360e0548a67
Balandala <balandala05@gmail.com>
2024-12-13 05:20:20 +0500

    Task 2 complete

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,29 @@
 package com.example.task02;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Task02Main {
 
     public static void main(String[] args) {
-
-        /*
-        cycleGrayCode(2)
+        cycleGrayCode(3)
                 .limit(10)
                 .forEach(System.out::println);
-        */
 
     }
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
+        if (n < 1 || n > 16)
+            throw new IllegalArgumentException("n must be within [1;16]");
 
+        int size = (int)Math.pow(2, n);
+        return IntStream.iterate(0, x -> (x + 1) % size).map(Task02Main::grayEncode);
+    }
+
+    static int grayEncode(int g)
+    {
+        return g ^ (g >> 1);
     }
 
 }
