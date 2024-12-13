08fd010dfc43adbd2dc96c3e06ff0f5d9f635ee9
Balandala <balandala05@gmail.com>
2024-12-13 05:21:42 +0500

    Task 3 complete

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -22,6 +22,33 @@ public class Task03Main {
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
 
-        // your implementation here
+        if (stream == null || order == null || minMaxConsumer == null)
+            throw new NullPointerException();
+
+        final T[] max = (T[]) new Object[1];
+        final T[] min = (T[]) new Object[1];
+        final boolean[] foundNull = new boolean[]{false};
+        stream.forEach(x ->
+                {
+                    if (x == null) {
+                        foundNull[0] = true;
+                        min[0] = null;
+                    }
+
+                    if (!foundNull[0] && min[0] == null && max[0] == null){
+                        min[0] = x;
+                        max[0] = x;
+                    }
+
+
+                    if (!foundNull[0] && order.compare(x, min[0]) < 0) {
+                        min[0] = x;
+                    }
+                    if (order.compare(x, max[0]) > 0)
+                        max[0] = x;
+                }
+
+        );
+        minMaxConsumer.accept(min[0], max[0]);
     }
 }
